b98c12d3a4dab5b375d006c642c885a93a8a9214
Yasuhisa Yoshida <syou6162@gmail.com>
2012-01-15 08:05:52 +0900

    1.1.8のletfnの例を追加

--- syou6162/sicp-practice/src/sicp_practice/1_1.clj
+++ syou6162/sicp-practice/src/sicp_practice/1_1.clj
@@ -64,3 +64,25 @@
 
 (defn sqrt [x]
   (sqrt-iter 1.0 x))
+
+
+;; 1.1.8 ブラックボックス抽象としての手続き
+
+;; Clojureの場合、defの中でdefしても外に見えてしまう&遅いので代わりにletfnを使うのが普通です
+;; ref http://d.hatena.ne.jp/fatrow/20100213/1266074787
+
+;; あるいは(defn- xxx)とやると名前空間の中のみで有効な関数が作れます
+
+(defn sqrt-1-1-8 [x]
+  (letfn [(average [x y]
+		   (/ (+ x y) 2))
+	  (improve [guess x]
+		   (average guess (/ x guess)))
+	  (square [x] (* x x))
+	  (good-enough? [guess x]
+			(< (Math/abs (- (square guess) x)) 0.001))
+	  (sqrt-iter [guess x]
+		     (if (good-enough? guess x)
+		       guess
+		       (sqrt-iter (improve guess x) x)))]
+    (sqrt-iter 1.0 x)))
--- syou6162/sicp-practice/test/sicp_practice/test/1_1.clj
+++ syou6162/sicp-practice/test/sicp_practice/test/1_1.clj
@@ -8,3 +8,7 @@
 (deftest test-sqrt
   (is (< (Math/abs (- 3.0 (sqrt 9))) 0.001))
   (is (< (Math/abs (- 11.704699917758145 (sqrt (+ 100 37)))) 0.001)))
+
+(deftest test-sqrt1-1-8
+  (is (< (Math/abs (- 3.0 (sqrt 9))) 0.001))
+  (is (< (Math/abs (- 11.704699917758145 (sqrt (+ 100 37)))) 0.001)))
