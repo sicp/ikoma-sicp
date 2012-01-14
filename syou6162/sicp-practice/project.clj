76918f47ab5f09175cac4ffb8e0e249758855cd1
Yasuhisa Yoshida <syou6162@gmail.com>
2012-01-15 04:23:48 +0900

    lein newして、練習問題をとりあえず追加

---
+++ syou6162/sicp-practice/project.clj
@@ -0,0 +1,5 @@
+(defproject sicp-practice "0.0.1-SNAPSHOT"
+  :description "syou6162のSICPの練習問題置き場"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+		 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.3.2"]])
