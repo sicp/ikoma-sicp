55d99e74101a3ba448d83e3a83bdd50f59dec32a
keisks <keisks5@gmail.com>
2012-02-11 00:36:21 +0900

    uploaded ex_1_2.clj, ex_1_8.clj

--- keisks/sicp-practice/project.clj
+++ keisks/sicp-practice/project.clj
@@ -1,3 +1,5 @@
 (defproject sicp-practice "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [vimclojure/server "2.3.1"]
+                 [org.clojars.ibdknox/lein-nailgun "1.1.1"]])
