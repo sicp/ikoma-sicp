66ce48c7d3f6e8639f32fdfb42304682986150dd
Yasuhisa Yoshida <syou6162@gmail.com>
2012-02-17 17:31:41 +0900

    両替問題を追加

--- syou6162/sicp-practice/src/sicp_practice/1_2.clj
+++ syou6162/sicp-practice/src/sicp_practice/1_2.clj
@@ -48,3 +48,26 @@
 
 (defn fib [n]
   (fib-iter 1 0 n))
+
+;; 両替の計算
+(defn first-denomination [kinds-of-coins]
+  (cond (= kinds-of-coins 1) 1
+	(= kinds-of-coins 2) 5
+	(= kinds-of-coins 3) 10
+	(= kinds-of-coins 4) 25
+	(= kinds-of-coins 5) 50))
+
+(defn cc [amount kinds-of-coins]
+  (cond (= amount 0) 1
+	(or (< amount 0) (= kinds-of-coins 0)) 0
+	:else (+ (cc amount
+		     (- kinds-of-coins 1))
+		 (cc (- amount
+			(first-denomination kinds-of-coins))
+		     kinds-of-coins))))
+
+(defn count-charge [amount]
+  (cc amount 5))
+
+(count-charge 100) ; 292
+
