38c8e483c855ea5edb3914d61c25934b64a8161e
yag_ays <yanagi.ayase@gmail.com>
2012-02-18 14:17:56 +0900

    solved 1.15

--- yag_ays/sicp-practice/project.clj
+++ yag_ays/sicp-practice/project.clj
@@ -1,3 +1,4 @@
 (defproject sicp-practice "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/tools.trace "0.7.1"]])
