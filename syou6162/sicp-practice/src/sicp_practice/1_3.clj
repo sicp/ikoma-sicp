c502324dd177b123f9dbb3917c0ea1fcefdab01e
Yasuhisa Yoshida <syou6162@gmail.com>
2012-03-03 05:38:35 +0900

    区間二分法と不動点法を実装(というか写経)

--- syou6162/sicp-practice/src/sicp_practice/1_3.clj
+++ syou6162/sicp-practice/src/sicp_practice/1_3.clj
@@ -1,4 +1,5 @@
-(ns sicp-practice.1-3)
+(ns sicp-practice.1-3
+  (:use [clojure.contrib.generic.math-functions :only (sin cos)]))
 
 ;; 引数としての手続き
 
@@ -30,3 +31,48 @@
 (integral #(* % % %) 0 1 0.01)
 (integral #(* % % %) 0 1 0.001)
 
+;; 1.3.3 一般的方法としての手続き
+
+(defn average [x y] (/ (+ x y) 2))
+
+(def eps 0.001)
+
+(defn close-enough? [x y]
+  (< (Math/abs (- x y)) eps))
+
+(defn search [f neg-point pos-point]
+  (let [midpoint (average neg-point pos-point)]
+    (if (close-enough? neg-point pos-point)
+      midpoint
+      (let [test-value (f midpoint)]
+	(cond (pos? test-value) (search f neg-point midpoint)
+	      (neg? test-value) (search f midpoint pos-point)
+	      :else midpoint)))))
+
+(defn half-interval-method [f a b]
+  (let [a-value (f a)
+	b-value (f b)]
+    (cond (and (neg? a-value) (pos? b-value)) (search f a b)
+	  (and (neg? b-value) (pos? a-value)) (search f b a)
+	  :else (throw (Exception. (str "Values are not of opposite sign" a b))))))
+
+(half-interval-method sin 2.0 4.0) ; 3.14111328125
+
+(half-interval-method (fn [x] (- (* x x x) (* 2 x) 3))
+		      1.0
+		      2.0) ; 1.89306640625
+
+(defn fixed-point [f first-guess]
+  (letfn [(close-enough? [v1 v2]
+			 (< (Math/abs (- v1 v2)) eps))
+	  (my-try [guess]
+		  (let [my-next (f guess)]
+		    (if (close-enough? guess my-next)
+		      my-next
+		      (my-try my-next))))]
+    (my-try first-guess)))
+
+(fixed-point cos 1.0) ; 0.7387603198742113
+
+(fixed-point (fn [y] (+ (sin y) (cos y)))
+	     1.0) ; 1.259003859740025
