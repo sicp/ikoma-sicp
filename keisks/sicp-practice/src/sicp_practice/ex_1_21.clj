76cb6876075283c4fddc07e683f993e222577af5
keisks <keisks5@gmail.com>
2012-03-02 14:49:32 +0900

    upload ex_1_21.clj, ex_1_26.clj

---
+++ keisks/sicp-practice/src/sicp_practice/ex_1_21.clj
@@ -0,0 +1,34 @@
+;ex_1_21
+
+(defn divides? [a b]
+  (= (rem b a) 0))
+
+(defn square [x]
+  (* x x))
+
+(defn find-divisor [n test-divisor]
+  (cond (> (square test-divisor) n) n
+        (divides? test-divisor n) test-divisor
+        :else (find-divisor n (+ test-divisor 1))))
+
+(defn smallest-divisor [n]
+  (find-divisor n 2))
+
+(smallest-divisor 199)
+;199
+(smallest-divisor 1999)
+;1000
+(smallest-divisor 19999)
+;7
+
+(defn prime? [n]
+  (= n (smallest-divisor n)))
+
+(prime? 2)
+;true
+(prime? 10)
+;false
+(prime? 13)
+;true
+(prime? 97)
+;true
