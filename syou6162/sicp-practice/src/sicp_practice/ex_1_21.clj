7ac22e0e202030e5aafa18ff9bf38225e60ccdd1
Yasuhisa Yoshida <syou6162@gmail.com>
2012-03-02 23:30:54 +0900

    またtypo...

--- syou6162/sicp-practice/src/sicp_practice/ex_1_21.clj
+++ syou6162/sicp-practice/src/sicp_practice/ex_1_21.clj
@@ -2,12 +2,12 @@
 
 (defn square [x] (* x x))
 
-(defn devides? [a b]
+(defn divides? [a b]
   (= (rem b a) 0))
 
 (defn find-divisor [n test-divisor]
   (cond (> (square test-divisor) n) n
-	(devides? test-divisor n) test-divisor
+	(divides? test-divisor n) test-divisor
 	:else (find-divisor n (+ test-divisor 1))))
 
 (defn smallest-divisor [n]
