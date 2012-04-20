c19b3750523dc34f872366b48fe4af19373db738
Yasuhisa Yoshida <syou6162@gmail.com>
2012-04-21 06:16:28 +0900

    図をplotする用にincanterの依存も追加

--- syou6162/sicp-practice/project.clj
+++ syou6162/sicp-practice/project.clj
@@ -3,5 +3,6 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
 		 [org.clojure/clojure-contrib "1.2.0"]
 		 [org.clojure/tools.trace "0.7.1"]
-		 [seesaw "1.2.2"]]
+		 [seesaw "1.2.2"]
+		 [incanter "1.3.0"]]
   :dev-dependencies [[swank-clojure "1.3.2"]])
