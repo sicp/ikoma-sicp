54657252bd3ef62fe77ec091c0077e84f6af0849
Yasuhisa Yoshida <syou6162@gmail.com>
2012-04-21 05:57:10 +0900

    問題2.18でネストしたリストもreverseが動くようにする

--- syou6162/sicp-practice/src/sicp_practice/ex_2_18.clj
+++ syou6162/sicp-practice/src/sicp_practice/ex_2_18.clj
@@ -17,3 +17,29 @@
 (my-reverse (range 5)) ; (4 3 2 1 0)
 (reduce conj '() (range 5)) ; (4 3 2 1 0)
 ; (conj (conj '() 1) 2) => (2 1)
+
+;;;; ここから自主課題 ;;;;;
+;; (ありがちだけど)ネストしててもreverseしてくれるような関数を書く
+
+(defn my-reverse-recursive [tree]
+  (letfn [(my-reverse-recursive'
+	   [tree acc]
+	   (if (empty? tree)
+	     acc
+	     (my-reverse-recursive'
+	      (rest tree)
+	      (cons (let [fst (first tree)] ;; ここだけ変更
+		      (if (list? fst)
+			(my-reverse-recursive' fst '())
+			fst))
+		    acc))))]
+    (my-reverse-recursive' tree '())))
+
+(my-reverse '(1 2 (3 4 (5 6) 7 8))) ; ((3 4 (5 6) 7 8) 2 1)
+(my-reverse-recursive '(1 2
+			  (3 4
+			     (5 6 7)
+			     8)
+			  9 10)) ; (10 9 (8 (7 6 5) 4 3) 2 1)
+;; もちろんflatなリストに対しても動く
+(my-reverse-recursive (range 5)) ; (4 3 2 1 0)
