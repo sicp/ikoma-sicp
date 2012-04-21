ea1f675257bd47c212755516e7a20f7b01272bfd
OZAWA Tsuyoshi <ozawa.tsuyoshi@gmail.com>
2012-04-21 09:57:58 +0900

    2: updated 17

--- oza/2/2-17.py
+++ oza/2/2-17.py
@@ -1,27 +1,69 @@
 
 # last_pair
 
-class mytuple:
+#class mytuple:
+#
+#	def __init__(self, t):
+#		self.tpl = t
+#
+#	def car(self):
+#		return self.tpl[0]
+#
+#	def cdr(self):
+#		return self.tpl[1:]
+#
+#	def last_pair(self):
+#		# return (self.tpl(-1),)
+#		l = len(self.tpl)
+#		return (self.tpl[l-1],)
+#
+#	def __str__(self):
+#		return str(self.tpl)
+#
+#t = mytuple((1,2))
+#print(t)
+#print(t.last_pair())
 
-	def __init__(self, t):
-		self.tpl = t
 
-	def car(self):
-		return self.tpl[0]
+def make_rlist(first, rest):
+	 return (first, rest)
 
-	def cdr(self):
-		return self.tpl[1:]
+def first(s):
+	return s[0]
 
-	def last_pair(self):
-		# return (self.tpl(-1),)
-		l = len(self.tpl)
-		return (self.tpl[l-1],)
+def rest(s):
+	return s[1]
 
-	def __str__(self):
-		return str(self.tpl)
 
-t = mytuple((1,2))
-print(t)
-print(t.last_pair())
-print(t.car())
-print(t.cdr())
+empty_rlist = None
+counts = make_rlist(1, make_rlist(2, make_rlist(3, make_rlist(4, empty_rlist))))
+print(counts)
+# (1, (2, (3, (4, None))))
+
+def len_rlist(s):
+	"""Return the length of recursive list s."""
+	length = 0
+	while s != empty_rlist:
+		s, length = rest(s), length + 1
+	return length
+
+
+def getitem_rlist(s, i):
+	"""Return the element at index i of recursive list s."""
+	while i > 0:
+		s, i = rest(s), i - 1
+	return first(s)
+
+print(len_rlist(counts))
+print(getitem_rlist(counts, 1))
+
+def last_pair(tpl):
+	if tpl[0] == empty_rlist:
+		return tpl
+
+	while tpl[1] != None:
+		tpl = rest(tpl)
+	return (tpl[0],)
+
+print(last_pair(counts))
+print(last_pair((None,)))
