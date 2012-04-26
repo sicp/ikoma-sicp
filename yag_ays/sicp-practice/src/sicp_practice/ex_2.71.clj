2202ee6b9144c75fb6ec6e6b447050d5b08fc7ed
yag_ays <yanagi.ayase@gmail.com>
2012-04-26 09:57:30 +0900

    typo修正と解説の追加

--- yag_ays/sicp-practice/src/sicp_practice/ex_2.71.clj
+++ yag_ays/sicp-practice/src/sicp_practice/ex_2.71.clj
@@ -21,9 +21,9 @@
                                       (F 32)
                                       (G 64)
                                       (H 128)
-                                      (I 258)
+                                      (I 256)
                                       (J 512))))
-;; ((leaf J 512)
+;; (
 ;;  (
 ;;   (
 ;;    (
@@ -39,8 +39,17 @@
 ;;      (leaf F 32) (A B C D E F) 63)
 ;;     (leaf G 64) (A B C D E F G) 127)
 ;;    (leaf H 128) (A B C D E F G H) 255)
-;;   (leaf I 258) (A B C D E F G H I) 513)
-;;  (J A B C D E F G H I) 1025)
+;;   (leaf I 256) (A B C D E F G H I) 511)
+;;  (leaf J 512) (A B C D E F G H I J) 1023)
 
 ;; 最高頻度の記号を符号化するのに必要なビット数は 1   (上の図のEやJ)
 ;; 最低頻度の記号を符号化するのに必要なビット数は n-1 (上の図のA)
+
+;; 上のfig.の通り，相対頻度が1,2,4,...2^{n-1}のようなn個のleafを持つHaffman treeは直線状に伸び，
+;; 常にright-branchから枝が伸びることはない．
+;; 重みの設定上，任意の正の整数nにおいて\sum _ {i}^{n-1}{2^i} < 2^nとなる．
+
+(right-branch n10tree)
+;; (leaf J 512)
+(left-branch n10tree)
+;; (((((((((leaf A 1) (leaf B 2) (A B) 3) (leaf C 4) (A B C) 7) (leaf D 8) (A B C D) 15) (leaf E 16) (A B C D E) 31) (leaf F 32) (A B C D E F) 63) (leaf G 64) (A B C D E F G) 127) (leaf H 128) (A B C D E F G H) 255) (leaf I 256) (A B C D E F G H I) 511)
